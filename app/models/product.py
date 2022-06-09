3db08c709e3b9531a7a01ea7ca6294426944076b
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2022-06-09 21:09:08 +0200

    homework 2022-06-09 KK

--- app/models/product.py
+++ app/models/product.py
@@ -1,3 +1,7 @@
 class Product():
-    def __init__():
-        pass
+    def __init__(self, product_id, all_opinions, stats):
+        self.product_id = product_id
+        self.all_opinions = all_opinions
+        self.stats = stats
+
+        
