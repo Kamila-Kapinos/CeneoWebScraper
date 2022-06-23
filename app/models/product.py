b025192e8f35241ded02ce924c321666ddd1ef81
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2022-06-23 19:14:28 +0200

    Tests

--- app/models/product.py
+++ app/models/product.py
@@ -13,6 +13,8 @@ plt.switch_backend('Agg')
 
 class Product:
     def __init__(self, product_id=0, opinions=[], product_name="", opinions_count=0, pros_count=0, cons_count=0, average_score=0):
+        
+        print('opinions', len(opinions))
         self.product_id = product_id
         self.product_name = product_name
         self.opinions = opinions
@@ -20,6 +22,7 @@ class Product:
         self.pros_count = pros_count
         self.cons_count = cons_count
         self.average_score = average_score
+        print('init prod', len(self.opinions))
     
     def __str__(self):
         return f"""product_id: {self.product_id}<br>
@@ -65,11 +68,22 @@ class Product:
         }
     
     def opinions_to_dict(self):
+        # print('=======')
+        # print(self.opinions)
+        # print('=======')
+        # print([opinion.to_dict() for opinion in self.opinions])
+
         return [opinion.to_dict() for opinion in self.opinions]
 
     def extract_product(self):
+
+        # self.opinions = []
+
         url = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
         response = requests.get(url)
+        
+        print(url, len(response.text))
+
         page = BeautifulSoup(response.text, 'html.parser')
         self.product_name = get_item(page, "h1.product-top__product-info__name")
         while(url):
@@ -82,41 +96,47 @@ class Product:
                 url = "https://www.ceneo.pl"+get_item(page,"a.pagination__next","href")
             except TypeError:
                 url = None
+
+        print('opinions', len(self.opinions))
         return self
     
     def opinions_to_df(self):
         opinions = pd.read_json(json.dumps([opinion.to_dict() for opinion in self.opinions]))
-        opinions.stars = opinions.stars.map(lambda x: float(x.split("/")[0].replace(",", ".")))
+        if not opinions.empty:
+            opinions.stars = opinions.stars.map(lambda x: float(x.split("/")[0].replace(",", ".")))
         return opinions
     
     def process_stats(self):
-        self.opinions_count = self.opinions_to_df().shape[0]
-        self.pros_count = self.opinions_to_df().pros.map(bool).sum()
-        self.cons_count = self.opinions_to_df().cons.map(bool).sum()
-        self.average_score = self.opinions_to_df().stars.mean().round(2)
+        df = self.opinions_to_df()
+        if not df.empty:
+            self.opinions_count = df.shape[0]
+            self.pros_count = df.pros.map(bool).sum()
+            self.cons_count = df.cons.map(bool).sum()
+            self.average_score = df.stars.mean().round(2)
         return self
 
     def draw_charts(self): 
-        
-        recommendation = self.opinions_to_df().recommendation.value_counts(dropna = False).sort_index().reindex(["Nie polecam", "Polecam", None])
-        recommendation.plot.pie(
-            label="", 
-            autopct="%1.1f%%", 
-            colors=["lightsalmon", "lightgreen", "lightskyblue"],
-            labels=["Nie polecam", "Polecam", "Nie mam zdania"]
-        )
-        plt.title("Rekomendacja")
-        plt.savefig(f"app/static/plots/{self.product_id}_recommendations.png")
-        plt.close()
-        stars = self.opinions_to_df().stars.value_counts().sort_index().reindex([x / 10.0 for x in range(5, 50, 5)], fill_value=0)
-        stars.plot.bar()
-        plt.title("Oceny produktu")
-        plt.xlabel("Liczba gwiazdek")
-        plt.ylabel("Liczba opinii")
-        plt.grid(True)
-        plt.xticks(rotation=0)
-        plt.savefig(f"app/static/plots/{self.product_id}_stars.png")
-        plt.close()
+        df = self.opinions_to_df()
+        if not df.empty:
+            recommendation = df.recommendation.value_counts(dropna = False).sort_index().reindex(["Nie polecam", "Polecam", None])
+            recommendation.plot.pie(
+                label="", 
+                autopct="%1.1f%%", 
+                colors=["lightsalmon", "lightgreen", "lightskyblue"],
+                labels=["Nie polecam", "Polecam", "Nie mam zdania"]
+            )
+            plt.title("Rekomendacja")
+            plt.savefig(f"app/static/plots/{self.product_id}_recommendations.png")
+            plt.close()
+            stars = df.stars.value_counts().sort_index().reindex([x / 10.0 for x in range(5, 50, 5)], fill_value=0)
+            stars.plot.bar()
+            plt.title("Oceny produktu")
+            plt.xlabel("Liczba gwiazdek")
+            plt.ylabel("Liczba opinii")
+            plt.grid(True)
+            plt.xticks(rotation=0)
+            plt.savefig(f"app/static/plots/{self.product_id}_stars.png")
+            plt.close()
 
     def save_opinions(self):        
         if not os.path.exists("app/opinions"):
